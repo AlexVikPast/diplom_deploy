fd61b0241fc81fc95df8a9c90dae4d250890fff3
Alex Past <steplerpav@gmail.com>
2023-06-01 02:18:50 +0700

    f

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     def welcome_email
         @email = params[:email]
         @code = params[:code]
-        @url  = "#{ENV['HOST']}/code?code=#{@code}&email=#{@email}"
+        # @url  = "#{ENV['HOST']}/code?code=#{@code}&email=#{@email}"
         mail(to: @email, subject: 'Welcome to Online Cinima')
     end
 end
